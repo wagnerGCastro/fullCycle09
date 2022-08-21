1b8b3ed14706d73ffd1aff47ef13c2a375049a92
Wagner Castro <wagner_gcastro@hotmail.com>
2022-08-21 18:10:18 -0300

    #06 - (outhers): ronaming files, and ignoring, reestart
    - default store-api
    - default  invoices-frontend
    - default invoices-api

--- invoices-frontend/src/http.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const http = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_INVOICE_API_URL
-})
-
-export default http;
--- store-api/.docker/postgres/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE store;
-CREATE DATABASE invoice;
-CREATE DATABASE codebank;
